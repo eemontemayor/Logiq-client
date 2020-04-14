ee215372c8039010e44a47a7fd47dcc60f46eaa0
eemontemayor <eemontemayor@live.com>
2020-04-14 17:28:56 -0500

    removed boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+// import Ops from './logicOps/Ops'
 function App() {
+
+
+  let symbs ={
+
+    conjunction: '&',
+    conditional:'->',
+    disjunction:'v',
+    negation:'~'
+  }
+
+
+  function renderOperatos(symbs){
+      let a = 'A'
+      let b = 'B'
+      for (const [key,value] in Object.entries(symbs)) {
+        if (symbs.hasOwnProperty(key)) {
+         return <p>
+
+           {a} {value} {b}
+         </p>
+          
+        }
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+    {renderOperatos(symbs)}
+    </main>
   );
 }
 
---
+++ src/logicOps/Ops.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Ops(){
+
+    
+   const symbs = ()=>{
+       return{
+        conjunction: '&',
+        conditional:'->',
+        disjunction:'v',
+        negation:'~'
+
+    }
+ 
+    }
+return(
+    symbs
+)
+
+}
+ 
